63bd2f037c6fe56171f99ee3bada90f64fe4052c
wasdeilonn <sjamsuddin.dilon@gmail.com>
2026-02-12 21:08:08 +0100

    done charge stuff idk

--- src/Main.cs
+++ src/Main.cs
@@ -4,6 +4,7 @@ using Il2CppInterop.Runtime.Injection;
 using Polytopia.Data;
 using UnityEngine;
 using UnityEngine.Rendering.RenderGraphModule.NativeRenderPassCompiler;
+//using Polibrary; <-- it would be so awesome, it would be so cool
 
 using Il2Gen = Il2CppSystem.Collections.Generic;
 
@@ -22,14 +23,38 @@ public static class Main
     }
 
     [HarmonyPostfix]
+    [HarmonyPatch(typeof(GameLogicData), nameof(GameLogicData.AddGameLogicPlaceholders))]
+    public static void GetEnumShit(Newtonsoft.Json.Linq.JObject rootObject)
+    {
+        if (
+            !EnumCache<UnitAbility.Type>.TryGetType("charge_ability_ancients", out var chargeType) 
+            || !EnumCache<UnitAbility.Type>.TryGetType("capacitor_ability_ancients", out var capacitorType) 
+            || !EnumCache<UnitEffect>.TryGetType("charge_effect_ancients", out var chargeEffectType)
+            || !EnumCache<ImprovementData.Type>.TryGetType("excavate_improvement_ancients", out var excavateType)
+            )
+		{
+			modLogger.LogInfo("couldnt find some enumcache shit");
+			return;
+		}
+
+        Charge = chargeType;
+        Capacitor = capacitorType;
+        Charged = chargeEffectType;
+        Excavate = excavateType;
+    }
+
+	public static Dictionary<UnitData.Type, int> MaxCharge = new Dictionary<UnitData.Type, int>();
+
+    public static UnitAbility.Type Charge;
+    public static UnitAbility.Type Capacitor;
+    public static UnitEffect Charged;
+    public static ImprovementData.Type Excavate;
+
+    [HarmonyPostfix]
     [HarmonyPatch(typeof(GameLogicData), nameof(GameLogicData.CanBuild))]
     public static void GLD_CanBuild(ref bool __result, GameState gameState, TileData tile, PlayerState playerState, ImprovementData improvement)
     {
-        if (!EnumCache<ImprovementData.Type>.TryGetType("excavate_improvement_ancients", out var type))
-        {
-            modLogger.LogInfo("excavate renamed but not updated in polyscript?");
-        }
-        if (improvement.type == type)
+        if (improvement.type == Excavate)
         {
             if (tile.improvement != null)
             {
@@ -38,28 +63,95 @@ public static class Main
         }
     }
 
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(UnitDataExtensions), nameof(UnitDataExtensions.GetAttackOptionsAtPosition))]
+    private static void RangeFix(Il2Gen.List<WorldCoordinates> __result, GameState gameState, byte playerId, WorldCoordinates position, ref int range, bool includeHiddenTiles = false, UnitState customUnitState = null, bool ignoreDiplomacyRelation = false)
+	{
+        UnitState unit = gameState.Map.GetTile(position).unit;
+        if (unit == null) return;
+
+        int newrange = range;
+        foreach (UnitEffect effect in unit.effects)
+        {
+            if (effect == Charged)
+            {
+                newrange++;
+            }
+        }
+
+        range = newrange;
+    }
+
 	[HarmonyPostfix]
     [HarmonyPatch(typeof(UnitDataExtensions), nameof(UnitDataExtensions.GetAttackOptionsAtPosition))]
-    private static void GetAttackOptionsAtPosition(Il2Gen.List<WorldCoordinates> __result, GameState gameState, byte playerId, WorldCoordinates position, int range, bool includeHiddenTiles = false, UnitState customUnitState = null, bool ignoreDiplomacyRelation = false)
+    private static void UnitDataExtensions_GetAttackOptionsAtPosition(Il2Gen.List<WorldCoordinates> __result, GameState gameState, byte playerId, WorldCoordinates position, int range, bool includeHiddenTiles = false, UnitState customUnitState = null, bool ignoreDiplomacyRelation = false)
 	{
+        UnitState unit = gameState.Map.GetTile(position).unit;
+        if (unit == null) return;
+
 		Il2Gen.List<TileData> list = gameState.Map.GetArea(position, range, true, false);
-
-		if (gameState.Map.GetTile(position).unit == null) return;
-		if (!EnumCache<UnitAbility.Type>.TryGetType("charge_ability_ancients", out var chargeType) || !EnumCache<UnitAbility.Type>.TryGetType("capacitor_ability_ancients", out var capacitorType)) return;
-
-		if (gameState.Map.GetTile(position).unit.HasAbility(chargeType))
+		if (unit.HasAbility(Charge))
 		{
 			foreach (TileData tile in list)
 			{
 				if (tile.unit == null) continue;
 
-				if (tile.unit.HasAbility(capacitorType))
+				if (tile.unit.HasAbility(Capacitor))
 				{
-					__result.Add(tile.coordinates);
+                    if (GetChargeCount(tile.unit) < GetMaxCharge(tile.unit.type))
+                    {
+                        __result.Add(tile.coordinates);
+                    }
 				}
 			}
 		}
 	}
+
+	[HarmonyPostfix]
+    [HarmonyPatch(typeof(AttackAction), nameof(AttackAction.Execute))]
+    private static void AttackAction_Execute(GameState state, AttackAction __instance)
+	{
+		if (state.Map.GetTile(__instance.Origin).unit == null) return;
+		if (state.Map.GetTile(__instance.Target).unit == null) return;
+
+		UnitState attacker = state.Map.GetTile(__instance.Origin).unit;
+		UnitState defender = state.Map.GetTile(__instance.Target).unit;
+
+		if (attacker.HasAbility(Charge) && defender.HasAbility(Capacitor))
+		{
+            defender.effects.Add(Charged);
+		}
+
+        if (attacker.HasAbility(Capacitor))
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                attacker.effects.Remove(Charged);
+            }
+        }
+	}
+
+    static int GetChargeCount(UnitState unit)
+    {
+        int charges = 0;
+
+        foreach (UnitEffect effect in unit.effects)
+        {
+            if (effect == Charged)
+            {
+                charges++;
+            }
+        }
+
+        return charges;
+    }
+
+    static int GetMaxCharge(UnitData.Type unit)
+    {
+        int i = 3;
+        MaxCharge.TryGetValue(unit, out i);
+        return i;
+    }
 }
 
 
